8aeb2a3ba22399d4cc37be949d5e1b6282bfeb26
kbaljit <35734426+kbaljit@users.noreply.github.com>
2018-01-23 10:59:47 -0500

    Add files via upload

---
+++ first.c
@@ -0,0 +1,17 @@
+#include "first.h"
+
+int main(int argc, char *argv[]) {
+        int a;
+	if (argc != 2){
+		printf("error\n");
+		exit(1);
+	}
+	a = atoi(argv[1]);
+	if (a % 2 == 1){
+		printf("odd\n");
+	}
+	else if (a % 2 == 0){
+		printf("even\n");
+	}
+	return 0;
+}
